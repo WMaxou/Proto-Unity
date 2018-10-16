cdfc73b616575fc82252243ee5ceddc3d10d06a2
Paul EL KHOURY <p.elkhoury@student.isartdigital.com>
2018-10-16 19:39:47 +0200

    drag and drop obstacle

---
+++ PrototypePFE/Assets/Scripts/ControllerVR.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Valve.VR;
+
+public class ControllerVR : MonoBehaviour
+{
+    public SteamVR_Input_Sources source;
+
+    IGrabeable collidingObject;
+    IGrabeable objectInHand;
+
+    public Vector3 Velocity { get
+        {
+            switch (source)
+            {
+                case SteamVR_Input_Sources.LeftHand:
+                    return SteamVR_Input._default.inActions.SkeletonLeftHand.GetVelocity(source);
+
+                case SteamVR_Input_Sources.RightHand:
+                    return SteamVR_Input._default.inActions.SkeletonRightHand.GetVelocity(source);
+
+                default:
+                    return Vector3.zero;
+            }
+        }
+    }
+
+    public Vector3 AngularVelocity { get
+        {
+            switch (source)
+            {
+                case SteamVR_Input_Sources.LeftHand:
+                    return SteamVR_Input._default.inActions.SkeletonLeftHand.GetAngularVelocity(source);
+
+                case SteamVR_Input_Sources.RightHand:
+                    return SteamVR_Input._default.inActions.SkeletonRightHand.GetAngularVelocity(source);
+
+                default:
+                    return Vector3.zero;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (SteamVR_Input._default.inActions.GrabPinch.GetStateDown(source))
+        {
+            if (collidingObject != null)
+                collidingObject.Grab(source, this);
+        }
+
+        if (SteamVR_Input._default.inActions.GrabPinch.GetStateUp(source))
+        {
+            if (collidingObject != null)
+                collidingObject.Release(source, this);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    if (collidingObject == null)
+    //    {
+    //        return;
+    //    }
+
+    //    collidingObject = null;
+    //}
+
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject != null)
+            return;
+
+        IGrabeable obj = col.gameObject.GetComponent<IGrabeable>();
+
+        if (obj != null)
+            collidingObject = obj;
+    }
+}
---
+++ PrototypePFE/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Valve.VR;
+
+public class Obstacle : MonoBehaviour, IGrabeable
+{
+    public void Grab(SteamVR_Input_Sources source, ControllerVR controller)
+    {
+        var joint = AddFixedJoint();
+        joint.connectedBody = controller.gameObject.GetComponent<Rigidbody>();
+
+        NavMeshObstacle obstacle = GetComponent<NavMeshObstacle>();
+
+        if (obstacle != null)
+            obstacle.enabled = false;
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    public void Release(SteamVR_Input_Sources source, ControllerVR controller)
+    {
+        var joint = GetComponent<FixedJoint>();
+        if (joint)
+        {
+            joint.connectedBody = null;
+            Destroy(joint);
+
+            GetComponent<Rigidbody>().velocity += controller.Velocity * 30f;
+            GetComponent<Rigidbody>().angularVelocity = controller.AngularVelocity;
+
+            NavMeshObstacle obstacle = GetComponent<NavMeshObstacle>();
+
+            if (obstacle != null)
+                obstacle.enabled = true;
+        }
+    }
+}
